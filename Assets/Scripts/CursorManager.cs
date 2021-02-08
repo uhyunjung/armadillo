dee9ca74c063762b6d6cbfd96b1471481273616b
h-5un <ran5dwd@gmail.com>
2021-02-09 02:22:56 +0900

    스킬 4번, 스킬아이콘 및 커서 적용

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    // 커스텀 마우스 커서
+    [SerializeField] Texture2D cursorImg;
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        Cursor.SetCursor(cursorImg, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
