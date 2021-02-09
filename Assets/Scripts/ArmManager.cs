f1e7a548135a611504f4fe4d3975d1b6c44b7c5f
jooyoung cha <45927229+colpang@users.noreply.github.com>
2021-02-10 04:06:27 +0900

    2번, 3번 적용
    2번 문어발 등장 적용
    3번 감속 배속 적용

--- Assets/Scripts/ArmManager.cs
+++ Assets/Scripts/ArmManager.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class ArmManager : MonoBehaviour
 {
+    /*
+     탄막 2번 스피커 등장 해제 함수입니다.
+     */
 
+    BulletBtn bulletBtn; // 탄막 버튼 스크립트
 
-
-    void Update()
+    private void Start()
     {
+        bulletBtn = GameObject.Find("BulletBtn").GetComponent<BulletBtn>();
+
+    }
+    private void Update()
+    {
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();        //스프라이트 렌더러 선언
+        Color color = spr.color;
+
+
+
+        if (bulletBtn.num == 1)             //활성화
+        {
+            Debug.Log("2번 탄환 실행");
+            color.a = 1f;
+            spr.color = color;            
+        }
+        else if (bulletBtn.num != 1)   //bulletBtn.num != 1일때 비활성화
+        {
+            Debug.Log("2번 탄환 종료");
+            color.a = 0f;
+            spr.color = color;
+        }
+
+
 
     }
 
