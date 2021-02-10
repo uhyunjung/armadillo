c8d9aaeb1a062b2e7508ce4d86f862f0a72ebe65
hanby-choi <hanby.choi@ewhain.net>
2021-02-10 18:01:42 +0900

    21.02.09 백업
    로비에서의 씬 이동 구현(룸->로비 포함)/1920*1080에 맞춰 전반적인 오브젝트(UI) 해상도 조정/스크립트 폴더 내에 Game, Lobby, Room 폴더 만들어 스크립트 구분/텍스트 깨짐 문제 해결/로비 패널 및 룸 프리펩 구현/연결 상태 나타내는 텍스트 추가 -> 방 이름, 인원수 동기화 구현 및 여러 플레이어 접속 테스트 필요(추가 예정) - RoomData 스크립트 수정 예정

---
+++ Assets/Scripts/Game/TimeSpeed.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeSpeed : MonoBehaviour
+{
+
+    public Sprite FastSprite;       //배속 sprite
+    public Sprite SlowSprite;       //감속 sprite
+    public Sprite normalSprite;     //보통 sprite
+    BulletBtn bulletBtn;  // 탄막 버튼 스크립트
+
+    bool check = true;
+    int selectMode = 0;
+
+    Controller_Time ct;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        bulletBtn = GameObject.Find("BulletBtn").GetComponent<BulletBtn>();
+        ct = GameObject.Find("TimeController").GetComponent<Controller_Time>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void update()
+    {
+        if (Input.GetMouseButtonDown(0) && bulletBtn.num == 2 && check)
+        {
+            selectMode = Random.Range(1, 3);    //랜덤으로 1 혹은 2의 정수 생성
+            startMode();
+        }
+    }
+
+    void modeReset()
+    {
+        spriteRenderer.sprite = normalSprite;
+        Time.timeScale = 1.0f;
+        ct.isFastSpeed = false;
+        ct.isSlowSpeed = false;
+
+
+    }
+
+
+    void fastslow(int selectMode)
+    {
+        // 2배속
+        if (selectMode == 1)
+        {
+            ct.isFastSpeed = true;
+            Time.timeScale = 2f;
+            spriteRenderer.sprite = FastSprite;
+        }
+        // 1배속
+        else if (selectMode == 2)
+        {
+            ct.isSlowSpeed = true;
+            Time.timeScale = 0.75f;
+            spriteRenderer.sprite = SlowSprite;
+        }
+    }
+
+    public void startMode()
+    {
+        StartCoroutine(Cooltime(10));
+    }
+
+    IEnumerator Cooltime(float cool)
+    {
+        check = false;
+        while (cool > 0.0f)
+        {
+            cool -= Time.deltaTime;
+            fastslow(selectMode);
+            yield return null;
+        }
+        modeReset();
+        check = true;
+    }
+
+
+}
+
+
---
+++ Assets/Scripts/Lobby/RoomData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomData : MonoBehaviour
+{
+    public string roomName = "";
+    public int playerCount = 0;
+    public int maxPlayer = 0;
+
+    [System.NonSerialized]
+    public Text roomDataText;
+
+    void Awake()
+    {
+        roomDataText = GetComponentInChildren<Text>();
+    }
+
+    public void UpdateInfo()
+    {
+        roomDataText.text = string.Format(" {0} [{1} / {2}]", roomName, playerCount.ToString("00"), maxPlayer);
+    }
+}
