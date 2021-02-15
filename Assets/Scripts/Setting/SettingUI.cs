7e3e614930e8dc5750310bc038f04606988dbb23
hanby-choi <hanby.choi@ewhain.net>
2021-02-15 16:34:00 +0900

    팝업창 2개 추가(방 이름 Null/방에 접속 중)
    연결상태 텍스트 크기 및 표시 영역 수정
    
    팝업창 2개 추가(방 이름을 입력하지 않았을 때/방에 접속 중일 때 - 이 상태에 해당하던 연결상태 텍스트는 삭제)
    
    현정님이 코드 고쳐주셔서(풀리퀘 25번) 유저 수의 변화가 생긴 방만 리스트 업데이트 되는 문제 수정됨
    
    게임이 시작된 방은 룸 리스트에 보이지 않음/인원 수가 다 찬 방은 참여 불가함(open/close 관련하여 현재 작동 상태 확인)

---
+++ Assets/Scripts/Setting/SettingUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingUI : MonoBehaviour
+{
+    public void Move2Start()
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
